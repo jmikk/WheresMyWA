123068db1ec11d83db00019ca52d8c1942ba3f7a
jmikk <buldog.buldog26@gmail.com>
2020-11-11 13:10:55 -0600

    Now WAAAY faster

--- FindmyWA.py
+++ FindmyWA.py
@@ -4,41 +4,38 @@ import csv
 from time import sleep
 import os
 import sys
+import re
 
 Version = 1
 print("This is Version: "+str(Version))
 
 UserAgent=input("Please enter your main nations name: ")
 filename="puppet.csv"
-
-names=[]
+names = set()
 NewListOfIssues = 'Find_my_WA.txt'
 with open("puppet.csv") as csv_file:
 	csv_reader = csv.reader(csv_file)
 	for row in csv_reader:
-		names.append(row[0])
+		names.add(row[0].lower().replace(" ", "_"))
 index=0
 
 if os.path.exists(NewListOfIssues):
   os.remove(NewListOfIssues)
 stop=0;
-for every in names:
-	if(stop==0):
-		every=every.replace(" ", "_")	
-		r = requests.get('https://www.nationstates.net/cgi-bin/api.cgi/', headers={'User-Agent': UserAgent}, params={'nation':every, 'q':'wa'})
-		sleep(.8)
-		#print(r.content)
-		soup = BeautifulSoup(r.content, "xml")
-		#print(soup)
-		WAStatus=soup.find('UNSTATUS')
-		with open(NewListOfIssues, 'a+') as f:
-			#print(WAStatus.text)
-			if(WAStatus.text == 'WA Member' or WAStatus.text == 'WA Delegate'):
-				f.writelines('***************I found a WA on: '+ every+"\n")
-				print('I found a WA on: '+ every)
-				stop=1
-			else:
-				f.writelines('I did not find a WA on: '+ every+"\n")
-				print(every+" is a " +WAStatus.text)      
+
+if(stop==0):
+	r = requests.get('https://www.nationstates.net/cgi-bin/api.cgi', headers={'User-Agent': UserAgent}, params={'wa':1,'q':'members'})
+	sleep(.8)
+	#print(r.content)
+	soup = BeautifulSoup(r.content, "xml")
+	Mem=soup.find('MEMBERS')
+	#print(Mem.text)
+	with open(NewListOfIssues, 'a+') as f:
+		i = 0
+		for i, match in enumerate(names.intersection(Mem.text.split(",")), 1):
+  			print("I found a WA on: " + match)
+  			f.writelines("I found a WA on: " + match)
+		if not i:
+  			print("I didn't find any WA puppets.")
 input('Found it press enter to close')
 
